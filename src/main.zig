e9a68d9869f1c8bc9f011549c940f5b1f8d76a4e
abcode <andrea.bertanzon@protonmail.com>
2023-03-30 22:27:27 +0200

    adds list filtering

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,31 @@ const c = @cImport({
     @cInclude("curses.h");
 });
 
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+
+const Todo = struct {
+    content: []const u8,
+    done: bool = false,
+};
+
+const Status = enum {
+    All,
+    Done,
+    Todo,
+};
+
 var quit: bool = false;
+var currentHighlight: i32 = 0;
+var selectedTab:Status = .Todo;
 
 pub fn main() !void {
     // initializes ncurses
     _ = c.initscr();
-    
+    _ = c.start_color();
+    _ = c.init_pair(REGULAR_PAIR, c.COLOR_WHITE, c.COLOR_BLACK);
+    _ = c.init_pair(HIGHLIGHT_PAIR, c.COLOR_BLACK, c.COLOR_WHITE);
+
     // sets ncurses so that it does not hang waiting user input
     _ = c.curs_set(0);
     _ = c.noecho();
@@ -18,33 +37,41 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var todoList = std.ArrayList([]const u8).init(allocator);
+    var todoList = std.ArrayList(Todo).init(allocator);
     defer todoList.deinit();
 
-    // TODO #1: read inputs from the user and convert them to command;
-    // TODO #2: read the todolist from a file and load it in the todoList arraylist
-    // TODO #3: add items to the todoList via getch() and addstr()
+    // TODO #3: read the todolist from a file and load it in the todoList arraylist
+    // TODO #4: add items to the todoList via getch() and addstr() and opening a sort of popup window nvim style
+
+    try todoList.append(Todo{ .content = "Buy new laptop" });
+    try todoList.append(Todo{ .content = "Finish application" });
+    try todoList.append(Todo{ .content = "have fun with zig!", .done = true });
 
     while (!quit) {
-        try todoList.append("Say hello to Martha");
-        try todoList.append("Give money to charity");
-        try todoList.append("Pay the bills");
-
-        var i: usize = 0;
         var index: i32 = 0;
-        while (i < todoList.items.len) : (i += 1) {
+        for (todoList.items) |item| {
+            switch (selectedTab) {
+                .All => {  },
+                .Done => if (!item.done) continue,
+                .Todo => if (item.done) continue,
+            }
+            var activePair = if (currentHighlight == index) HIGHLIGHT_PAIR else REGULAR_PAIR;
+
+            _ = c.attron(c.COLOR_PAIR(activePair));
             _ = c.move(index, 0);
-            _ = c.addstr("[ ] ");
-            _ = c.addstr(todoList.items[i].ptr);
+
+            if (item.done) {
+                _ = c.addstr("[x] ");
+            } else {
+                _ = c.addstr("[ ] ");
+            }
+            _ = c.addstr(item.content.ptr);
+            _ = c.attroff(c.COLOR_PAIR(activePair));
             index += 1;
         }
+
         var char = c.getch();
-        switch (char) {
-            'q' => {
-                quit = true;
-            },
-            else => {},
-        }
+        handleUserInput(char, &todoList);
 
         // refreshes the screen and clears it adding the new added things since the last refresh
         _ = c.refresh();
@@ -52,6 +79,28 @@ pub fn main() !void {
 
     // releases all the structures created by ncurses and associated with it.
     _ = c.endwin();
+}
+
+pub fn handleUserInput(char: i32, todoList: *std.ArrayList(Todo)) void {
+    switch (char) {
+        'q' => {
+            quit = true;
+        },
+        'j' => {
+            if (currentHighlight < todoList.items.len - 1) {
+                currentHighlight += 1;
+            }
+        },
+        'k' => {
+            if (currentHighlight > 0) {
+                currentHighlight -= 1;
+            }
+        },
+        ' ' => {
+            todoList.items[@intCast(usize, currentHighlight)].done = !todoList.items[@intCast(usize, currentHighlight)].done;
+        },
+        else => {},
+    }
 }
 
 test "simple test" {
